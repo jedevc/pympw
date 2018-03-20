ca019f441748a6488f03abc0a8cada619361456f
jedevc <jedevc@gmail.com>
2018-03-20 00:01:56 -0400

    Fix problems with license headers.

--- mpw/mpw.py
+++ mpw/mpw.py
@@ -1,20 +1,20 @@
 # =============================================================================
 #
-#  Copyright (c) 2011-2017, Justin Chadwell.
-# 
+#  Copyright (c) 2017, Justin Chadwell.
+#
 #  This program is free software: you can redistribute it and/or modify it
 #  under the terms of the GNU General Public License as published by the Free
 #  Software Foundation, either version 3 of the License, or (at your option)
 #  any later version.
-# 
+#
 #  This program is distributed in the hope that it will be useful, but WITHOUT
 #  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
 #  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
 #  more details.
-# 
+#
 #  You can find a copy of the GNU General Public License in the LICENSE file.
 #  Alternatively, see <http://www.gnu.org/licenses/>.
-# 
+#
 # =============================================================================
 
 import argparse
