d2e47c5a4333c96fdbaab685f65e1370349d627f
Justin Chadwell <jedevc@gmail.com>
2018-04-08 21:08:38 +0700

    Move tests into classes.

--- test/test_algorithm.py
+++ test/test_algorithm.py
@@ -21,192 +21,194 @@ import unittest
 
 import mpw.algorithm
 
-class TestAlgorithm(unittest.TestCase):
-    def helper(self, version=-1, name='Robert Lee Mitchell',
-            master_password='banana colored duckling',
-            site='masterpasswordapp.com', counter=1, template='long'):
-        gen = mpw.algorithm.Algorithm(version)
-        key = gen.generate_key(master_password, name)
-        site_password = gen.generate_password(key, site, counter, template)
+# tests adopted from
+# https://github.com/Lyndir/MasterPassword/blob/master/core/mpw_tests.xml
 
-        return site_password
+def helper(version=-1, name='Robert Lee Mitchell',
+        master_password='banana colored duckling',
+        site='masterpasswordapp.com', counter=1, template='long'):
+    gen = mpw.algorithm.Algorithm(version)
+    key = gen.generate_key(master_password, name)
+    site_password = gen.generate_password(key, site, counter, template)
 
-    # Algorithm v0
-    def test_v0(self):
-        password = self.helper(version=0)
+    return site_password
+
+class TestAlgorithmV0(unittest.TestCase):
+    def test_core(self):
+        password = helper(version=0)
         self.assertEqual(password, 'Feji5@ReduWosh')
 
-    def test_v0_name(self):
-        password = self.helper(version=0, name='⛄')
+    def test_name(self):
+        password = helper(version=0, name='⛄')
         self.assertEqual(password, 'HajrYudo7@Mamh')
 
-    def test_v0_master_password(self):
-        password = self.helper(version=0, master_password='⛄')
+    def test_master_password(self):
+        password = helper(version=0, master_password='⛄')
         self.assertEqual(password, 'MewmDini0]Meho')
 
-    def test_v0_site(self):
-        password = self.helper(version=0, site='⛄')
+    def test_site(self):
+        password = helper(version=0, site='⛄')
         self.assertEqual(password, 'HahiVana2@Nole')
 
-    def test_v0_type_maximum(self):
-        password = self.helper(version=0, template='maximum')
+    def test_template_maximum(self):
+        password = helper(version=0, template='maximum')
         self.assertEqual(password, 'w1!3bA3icmRAc)SS@lwl')
 
-    def test_v0_type_medium(self):
-        password = self.helper(version=0, template='medium')
+    def test_template_medium(self):
+        password = helper(version=0, template='medium')
         self.assertEqual(password, 'Fej7]Jug')
 
-    def test_v0_type_basic(self):
-        password = self.helper(version=0, template='basic')
+    def test_template_basic(self):
+        password = helper(version=0, template='basic')
         self.assertEqual(password, 'wvH7irC1')
 
-    def test_v0_type_short(self):
-        password = self.helper(version=0, template='short')
+    def test_template_short(self):
+        password = helper(version=0, template='short')
         self.assertEqual(password, 'Fej7')
 
-    def test_v0_type_pin(self):
-        password = self.helper(version=0, template='pin')
+    def test_template_pin(self):
+        password = helper(version=0, template='pin')
         self.assertEqual(password, '2117')
 
-    def test_v0_type_name(self):
-        password = self.helper(version=0, template='name')
+    def test_template_name(self):
+        password = helper(version=0, template='name')
         self.assertEqual(password, 'fejrajugo')
 
-    def test_v0_type_phrase(self):
-        password = self.helper(version=0, template='phrase')
+    def test_template_phrase(self):
+        password = helper(version=0, template='phrase')
         self.assertEqual(password, 'fejr jug gabsibu bax')
 
-    # Algorithm v1
-    def test_v1(self):
-        password = self.helper(version=1)
+class TestAlgorithmV1(unittest.TestCase):
+    def test_core(self):
+        password = helper(version=1)
         self.assertEqual(password, 'Jejr5[RepuSosp')
 
-    def test_v1_name(self):
-        password = self.helper(version=1, name='⛄')
+    def test_name(self):
+        password = helper(version=1, name='⛄')
         self.assertEqual(password, 'WaqoGuho2[Xaxw')
 
-    def test_v1_master_password(self):
-        password = self.helper(version=1, master_password='⛄')
+    def test_master_password(self):
+        password = helper(version=1, master_password='⛄')
         self.assertEqual(password, 'QesuHirv5-Xepl')
 
-    def test_v1_site(self):
-        password = self.helper(version=1, site='⛄')
+    def test_site(self):
+        password = helper(version=1, site='⛄')
         self.assertEqual(password, 'WawiYarp2@Kodh')
 
-    def test_v1_type_maximum(self):
-        password = self.helper(version=1, template='maximum')
+    def test_template_maximum(self):
+        password = helper(version=1, template='maximum')
         self.assertEqual(password, 'W6@692^B1#&@gVdSdLZ@')
 
-    def test_v1_type_medium(self):
-        password = self.helper(version=1, template='medium')
+    def test_template_medium(self):
+        password = helper(version=1, template='medium')
         self.assertEqual(password, 'Jej2$Quv')
 
-    def test_v1_type_basic(self):
-        password = self.helper(version=1, template='basic')
+    def test_template_basic(self):
+        password = helper(version=1, template='basic')
         self.assertEqual(password, 'WAo2xIg6')
 
-    def test_v1_type_short(self):
-        password = self.helper(version=1, template='short')
+    def test_template_short(self):
+        password = helper(version=1, template='short')
         self.assertEqual(password, 'Jej2')
 
-    def test_v1_type_pin(self):
-        password = self.helper(version=1, template='pin')
+    def test_template_pin(self):
+        password = helper(version=1, template='pin')
         self.assertEqual(password, '7662')
 
-    def test_v1_type_name(self):
-        password = self.helper(version=1, template='name')
+    def test_template_name(self):
+        password = helper(version=1, template='name')
         self.assertEqual(password, 'jejraquvo')
 
-    def test_v1_type_phrase(self):
-        password = self.helper(version=1, template='phrase')
+    def test_template_phrase(self):
+        password = helper(version=1, template='phrase')
         self.assertEqual(password, 'jejr quv cabsibu tam')
 
-    # Algorithm v2
-    def test_v2(self):
-        password = self.helper(version=2)
+class TestAlgorithmV2(unittest.TestCase):
+    def test_core(self):
+        password = helper(version=2)
         self.assertEqual(password, 'Jejr5[RepuSosp')
 
-    def test_v2_name(self):
-        password = self.helper(version=2, name='⛄')
+    def test_name(self):
+        password = helper(version=2, name='⛄')
         self.assertEqual(password, 'WaqoGuho2[Xaxw')
 
-    def test_v2_master_password(self):
-        password = self.helper(version=2, master_password='⛄')
+    def test_master_password(self):
+        password = helper(version=2, master_password='⛄')
         self.assertEqual(password, 'QesuHirv5-Xepl')
 
-    def test_v2_site(self):
-        password = self.helper(version=2, site='⛄')
+    def test_site(self):
+        password = helper(version=2, site='⛄')
         self.assertEqual(password, 'LiheCuwhSerz6)')
 
-    def test_v2_type_maximum(self):
-        password = self.helper(version=2, template='maximum')
+    def test_template_maximum(self):
+        password = helper(version=2, template='maximum')
         self.assertEqual(password, 'W6@692^B1#&@gVdSdLZ@')
 
-    def test_v2_type_medium(self):
-        password = self.helper(version=2, template='medium')
+    def test_template_medium(self):
+        password = helper(version=2, template='medium')
         self.assertEqual(password, 'Jej2$Quv')
 
-    def test_v2_type_basic(self):
-        password = self.helper(version=2, template='basic')
+    def test_template_basic(self):
+        password = helper(version=2, template='basic')
         self.assertEqual(password, 'WAo2xIg6')
 
-    def test_v2_type_short(self):
-        password = self.helper(version=2, template='short')
+    def test_template_short(self):
+        password = helper(version=2, template='short')
         self.assertEqual(password, 'Jej2')
 
-    def test_v2_type_pin(self):
-        password = self.helper(version=2, template='pin')
+    def test_template_pin(self):
+        password = helper(version=2, template='pin')
         self.assertEqual(password, '7662')
 
-    def test_v2_type_name(self):
-        password = self.helper(version=2, template='name')
+    def test_template_name(self):
+        password = helper(version=2, template='name')
         self.assertEqual(password, 'jejraquvo')
 
-    def test_v2_type_phrase(self):
-        password = self.helper(version=2, template='phrase')
+    def test_template_phrase(self):
+        password = helper(version=2, template='phrase')
         self.assertEqual(password, 'jejr quv cabsibu tam')
 
-    # Algorithm v3
-    def test_v3(self):
-        password = self.helper(version=3)
+class TestAlgorithmV3(unittest.TestCase):
+    def test_core(self):
+        password = helper(version=3)
         self.assertEqual(password, 'Jejr5[RepuSosp')
 
-    def test_v3_name(self):
-        password = self.helper(version=3, name='⛄')
+    def test_name(self):
+        password = helper(version=3, name='⛄')
         self.assertEqual(password, 'NopaDajh8=Fene')
 
-    def test_v3_master_password(self):
-        password = self.helper(version=3, master_password='⛄')
+    def test_master_password(self):
+        password = helper(version=3, master_password='⛄')
         self.assertEqual(password, 'QesuHirv5-Xepl')
 
-    def test_v3_site(self):
-        password = self.helper(version=3, site='⛄')
+    def test_site(self):
+        password = helper(version=3, site='⛄')
         self.assertEqual(password, 'LiheCuwhSerz6)')
 
-    def test_v3_type_maximum(self):
-        password = self.helper(version=3, template='maximum')
+    def test_template_maximum(self):
+        password = helper(version=3, template='maximum')
         self.assertEqual(password, 'W6@692^B1#&@gVdSdLZ@')
 
-    def test_v3_type_medium(self):
-        password = self.helper(version=3, template='medium')
+    def test_template_medium(self):
+        password = helper(version=3, template='medium')
         self.assertEqual(password, 'Jej2$Quv')
 
-    def test_v3_type_basic(self):
-        password = self.helper(version=3, template='basic')
+    def test_template_basic(self):
+        password = helper(version=3, template='basic')
         self.assertEqual(password, 'WAo2xIg6')
 
-    def test_v3_type_short(self):
-        password = self.helper(version=3, template='short')
+    def test_template_short(self):
+        password = helper(version=3, template='short')
         self.assertEqual(password, 'Jej2')
 
-    def test_v3_type_pin(self):
-        password = self.helper(version=3, template='pin')
+    def test_template_pin(self):
+        password = helper(version=3, template='pin')
         self.assertEqual(password, '7662')
 
-    def test_v3_type_name(self):
-        password = self.helper(version=3, template='name')
+    def test_template_name(self):
+        password = helper(version=3, template='name')
         self.assertEqual(password, 'jejraquvo')
 
-    def test_v3_type_phrase(self):
-        password = self.helper(version=3, template='phrase')
+    def test_template_phrase(self):
+        password = helper(version=3, template='phrase')
         self.assertEqual(password, 'jejr quv cabsibu tam')
